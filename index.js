ad5e7b8d2cd524b103f0dd7d60bf08d740f6f7ef
delucis <swithinbank@gmail.com>
2017-08-14 17:36:22 +0200

    Replace .get() with NPM package “got”.

--- index.js
+++ index.js
@@ -2,46 +2,9 @@
  * @module reading-data-json-loader
  */
 
-const HTTP = require('http')
-const HTTPS = require('https')
-const { URL } = require('url')
+const GOT = require('got')
 
 const ReadingDataJSONLoader = (function () {
-  let get = function (urlString, config) {
-    let url = new URL(urlString)
-    let get = url.protocol === 'https:' ? HTTPS.get : HTTP.get
-    return new Promise(function (resolve, reject) {
-      get(url, (res) => {
-        const { statusCode, headers } = res
-        const contentType = headers['content-type']
-        let error
-        if (statusCode !== 200) {
-          error = new Error('Request Failed.\n' +
-                            `Status Code: ${statusCode}`)
-        } else if (config.validateContentType && !/^application\/json/.test(contentType)) {
-          error = new Error('Invalid content-type.\n' +
-                            `Expected application/json but received ${contentType}`)
-        }
-        if (error) {
-          console.error(error.message)
-          res.resume() // consume response data to free up memory
-          return
-        }
-
-        res.setEncoding('utf8')
-        let rawData = ''
-        res.on('data', (chunk) => { rawData += chunk })
-        res.on('end', () => {
-          try {
-            const parsedData = JSON.parse(rawData)
-            resolve(parsedData)
-          } catch (e) {
-            reject('Failed to parse response as JSON.\n' + e)
-          }
-        })
-      })
-    })
-  }
   return {
     config: {
       scope: 'json-loader',
@@ -61,7 +24,8 @@ const ReadingDataJSONLoader = (function () {
         throw new Error('ReadingDataJSONLoader#fetch(): expected config.path to be a string or an object with scope "' + scope + '".')
       }
       try {
-        let json = await get(path, config)
+        let res = await GOT(path, { json: true })
+        let json = res.body
         return json
       } catch (e) {
         throw new Error('ReadingDataJSONLoader#fetch(): ' + e)
